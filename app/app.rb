a55d85e5139209318ed728234838244abfb52b4f
Anna <anna.hollandsmith3@gmail.com>
2016-06-27 17:42:32 +0100

    set up user controller and created Rakefile :sweat_smile:

--- app/app.rb
+++ app/app.rb
@@ -2,11 +2,13 @@ ENV["RACK_ENV"] ||= "development"
 
 require 'sinatra/base'
 require_relative 'data_mapper_setup'
+require_relative 'controllers/user'
 
 class MakersBnB < Sinatra::Base
+set :views, File.expand_path('../views', __FILE__)
   get '/' do
     'Hello MakersBnB!'
-  end
+end
 
   # start the server if ruby file executed directly
   run! if app_file == $0

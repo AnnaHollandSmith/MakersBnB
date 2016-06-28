f265a52fe71c984767ac81ab3945783e493947e3
awye765 <awye765@googlemail.com>
2016-06-28 15:24:57 +0100

    #3 Multiple spaces - users can list multiple spaces

--- spec/feature/listing_space_spec.rb
+++ spec/feature/listing_space_spec.rb
@@ -19,9 +19,9 @@ feature 'Listing spaces' do
       fill_in 'Description', with: 'It has no wifi but it has bugs'
       fill_in 'Price', with: '1'
       click_button 'List my Space'
-      user = User.first
-      expect(user.spaces.map(&:name)).to include("My ugly smelly home")
-      expect(user.spaces.map(&:name)).to include("My beautiful home")
+      spaces = Space.all
+      expect(spaces.map(&:name)).to include("My ugly smelly home")
+      expect(spaces.map(&:name)).to include("My beautiful home")
     end
 
 end

8d7bf4cd6df9bd8eb1f6e7f782a988558411bfe4
Anna <anna.hollandsmith3@gmail.com>
2016-06-29 11:13:18 +0100

    #13 completed functionality for signing out

--- app/controllers/session.rb
+++ app/controllers/session.rb
@@ -1,20 +1,4 @@
 class MakersBnB < Sinatra::Base
-  get '/' do
-    @user = User.new
-    erb :'/index'
-  end
-
-  post '/new' do
-    @user = User.create(email: params[:email], password: params[:password], password_confirmation: params[:password_confirmation])
-    if @user.save
-      session[:user_id] = @user.id
-      redirect '/spaces'
-    else
-      flash.now[:errors] = @user.errors.full_messages
-      erb :'/index'
-    end
-  end
-
   get '/sessions/new' do
     erb :'/sessions/new'
   end
@@ -35,5 +19,4 @@ class MakersBnB < Sinatra::Base
     flash.keep[:notice] = 'goodbye'
     redirect '/'
   end
-
-  end
+end

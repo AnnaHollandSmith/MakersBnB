909b0ca18a3c3e9453f75e357c2f183b717554d4
kpuwal <kpuwal@gmail.com>
2016-06-30 16:40:31 +0100

    #7 request approval not working :cry:

--- app/controllers/request.rb
+++ app/controllers/request.rb
@@ -13,28 +13,21 @@ class MakersBnB < Sinatra::Base
   get '/requests' do
     @requests_made = Request.all(user: current_user)
     @requests_received = Request.all(space: Space.all(user: current_user))
-    @confirm_view = session[:confirm_view]
     erb :'requests/index'
   end
 
-  post '/confirmation' do
-    session[:confirm_view] = true
-    @confirm_view = session[:confirm_view]
-    session[:current_request] = request
-     redirect('/requests')
+  get '/requests/:id' do
+    @request = Request.get(params[:id])
+    erb :'confirmation/new'
   end
 
+  put '/requests/:id' do
+
+  end
   post '/approve' do
-    session[:confirm_view] = false
-    session[:current_request].approval = 'Approved'
     redirect('/requests')
   end
 
-  post '/deny' do
-    session[:confirm_view] = false
-    session[:current_request].approval = 'Denied'
-    redirect('/requests')
-  end
 
 # @current_request = Request.all(space: Space.all(user: current_user))
 

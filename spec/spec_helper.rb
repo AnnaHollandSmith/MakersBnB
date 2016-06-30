f0d2a28dd12c15ea70ea1e02e1f0b9a9a05a3f43
riyapabari <riya.pabari@gmail.com>
2016-06-30 09:54:54 +0100

    updated database cleaner in spec_helper :bowtie:

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -32,7 +32,7 @@ RSpec.configure do |config|
 
   config.before(:suite) do
     DatabaseCleaner.strategy = :transaction
-    DatabaseCleaner.clean_with(:transaction)
+    DatabaseCleaner.clean_with(:truncation)
   end
 
   config.before(:each) do
